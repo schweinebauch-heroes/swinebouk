91990dba641f173a99bdf2c53287ecd7c74ebd9b
Johnny Lee-Othon <jleeothon@icloud.com>
2018-04-18 23:29:24 +0200

    Added language specification schemas and parsers

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 module.exports = {
-	Articulation: require('./lib/articulation'),
-	Vowel: require('./lib/vowel'),
-	VowelChart: require('./lib/vowel-chart'),
-	VowelLang: require('./lib/vowel-lang'),
-	VowelList: require('./lib/vowel-list'),
-	VowelPlotter: require('./lib/vowel-plotter'),
+	InputParser: require('./lib/input-parser'),
+	QueryParser: require('./lib/query-parser'),
+	SchemaValidator: require('./lib/schema-validator'),
 };
---
+++ test/input-parser.js
@@ -0,0 +1,74 @@
+import test from 'ava';
+
+import InputParser from '../lib/input-parser';
+import ParseError from '../lib/input-parse-error';
+
+const parser = new InputParser();
+
+test('Parse correctly', t => {
+	const input = {
+		vowels: [
+			{front: 1, close: 1, round: true, label: 'y'},
+			{front: 0, close: 0, round: false, 'x-sampa': 'A'},
+		],
+	};
+	const expected = {
+		vowels: [
+			{front: 1, close: 1, round: true, label: 'y'},
+			{front: 0, close: 0, round: false, label: 'ɑ'},
+		],
+	};
+	const result = parser.parse(input);
+	t.deepEqual(result, expected);
+});
+
+function testMissingRequiredField(t, input, errorClass) {
+	t.throws(() => {
+		parser.parse(input);
+	}, errorClass);
+}
+
+test(
+	'Missing basic vowel quality',
+	testMissingRequiredField,
+	{
+		vowels: [{front: 1, close: 1, label: 't'}],
+	},
+	ParseError
+);
+
+test(
+	'Missing label',
+	testMissingRequiredField,
+	{
+		vowels: [{front: 1, close: 1, round: 1}],
+	},
+	ParseError
+);
+
+test(
+	'Too many labels',
+	testMissingRequiredField,
+	{
+		vowels: [{front: 1, close: 1, round: 1, label: 'x', 'x-sampa': 'x'}],
+	},
+	ParseError
+);
+
+test(
+	'Front value too big',
+	testMissingRequiredField,
+	{
+		vowels: [{front: 1.1, close: 1, round: 1, label: 'x', 'x-sampa': 'x'}],
+	},
+	ParseError
+);
+
+test(
+	'Front value too small',
+	testMissingRequiredField,
+	{
+		vowels: [{front: -1, close: 1, round: 1, label: 'x', 'x-sampa': 'x'}],
+	},
+	ParseError
+);
