5de1e9d3fc8a32469eb77479b136f2640365d6b2
Johnny Lee-Othon <jleeothon@icloud.com>
2018-12-31 18:43:03 +0100

    updated definitions to include fractions

--- test/query-parser.js
+++ test/query-parser.js
@@ -4,28 +4,50 @@ import QueryParser from '../lib/query-parser';
 
 const parser = new QueryParser();
 
-test('Parse single vowel', parse, 'v=f1c1ula', {
+test('Parse single vowel', parse, 'v=f1c0ula', {
 	vowels: [
 		{
-			close: 1,
 			front: 1,
+			close: 0,
 			round: false,
 			label: 'a',
 		},
 	],
 });
 
-test('Parse two vowels', parse, 'v=f1c1ula&v=f12c12uxi', {
+test('Parse vowel with decimals', parse, 'v=f.5c0.5ulə', {
 	vowels: [
 		{
-			close: 1,
+			front: 0.5,
+			close: 0.5,
+			round: false,
+			label: 'ə',
+		},
+	],
+});
+
+test('Parse vowel with fraction', parse, 'v=f1/2c1/2ulə', {
+	vowels: [
+		{
+			front: '1/2',
+			close: '1/2',
+			round: false,
+			label: 'ə',
+		},
+	],
+});
+
+test('Parse more vowels', parse, 'v=f1c0ula&v=f1c1uxi', {
+	vowels: [
+		{
 			front: 1,
+			close: 0,
 			round: false,
 			label: 'a',
 		},
 		{
-			close: 12,
-			front: 12,
+			front: 1,
+			close: 1,
 			round: false,
 			'x-sampa': 'i',
 		},
